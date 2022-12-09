13fc94b80f11f7e9010d0f692a4d0c358b50ff76
ashleypereira <ashleygrace009@yahoo.com>
2022-12-08 22:01:47 -0500

    updatrd rsolvers and typeDef files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,7 +14,52 @@ const resolvers = {
         throw new AuthenticationError("Not logged in");
       },
   },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      // check if user exists with email and credentials
+      if (!user) {
+        throw new AuthenticationError("Credentials are incorrect");
+      }
+      const correctPassword = await user.isCorrectPassword(password);
 
+      // check password
+      if (!correctPassword) {
+        throw new AuthenticationError("Credentials are incorrect");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("Need to be logged in");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("Need to be logged in");
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,21 +8,34 @@ type User {
     bookCount: Int
     savedBooks: [Book]
   }
+
   type Book {
-    bookId: ID
+    bookId: String
     authors: [String]
     description: String
     title: String
     image: String
     link: String
   }
+
   type Auth {
     token: ID!
     user: User
   }
+
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+
   type Query {
     me: User
   }
+  
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
